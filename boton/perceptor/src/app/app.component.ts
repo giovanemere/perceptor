97629f1a3e7f6b632719df3a545e7988b577ea93
Edisson Zuñiga <edisson.zuniga@bancolombia.com>
2023-01-27 16:32:38 -0500

    push file angular

--- boton/perceptor/src/app/app.component.ts
+++ boton/perceptor/src/app/app.component.ts
@@ -1,38 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, catchError, of } from "rxjs";
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
 
-  public url: string = 'http://192.168.137.5:8091/devops/jenkins/job/FOLDER_OPSDEV/job/SCCOLCOMUNES/job/OPTIMIZACION/job/FREE_STYLE/job/TRIGGER_MACROS_SIN_PARAMS/build?token=11a78018ef44bdc4ebcce077bbcefa36eb';
+export class AppComponent implements OnInit {
+
+  form!: UntypedFormGroup;
+  chosenFileName: any;
+  chosenFile: any;
+  viewMode = 'deshabilitado';
+
+  public serverUrl: string = 'http://192.168.137.5:8091/devops/jenkins/job/FOLDER_OPSDEV/job/SCCOLCOMUNES/job/OPTIMIZACION/job/FREE_STYLE/job/TRIGGER_MACROS/buildWithParameters';
 
   constructor(
-    public http: HttpClient
-  ) {  }
-  
-  peticion()
-    {
-      this.login().subscribe(res => (
-        console.log('peticion')
-      ), (error:any) => (console.log(error)));
-    }
+    public http: HttpClient,
+    private fb: UntypedFormBuilder
+  ) { }
 
-  login():Observable<any>{
-    console.log('entro al login');
-    
-      const headerOptions = {
-           headers: new HttpHeaders({
-            'Authorization': 'Basic Z2lvdmFuZW1lcmU6MTFhNzgwMThlZjQ0YmRjNGViY2NlMDc3YmJjZWZhMzZlYg=='
-           })
-      };
-      
-      return this.http.post('http://192.168.137.5:8091/devops/jenkins/job/FOLDER_OPSDEV/job/SCCOLCOMUNES/job/OPTIMIZACION/job/FREE_STYLE/job/TRIGGER_MACROS_SIN_PARAMS/build','', headerOptions);
-  
+  peticion() {
+    // this.login().subscribe(res => (
+    //   console.log('peticion')
+    // ), (error:any) => (console.log(error)));
   }
+
+  login(): Observable<any> {
+    console.log('entro al login');
+
+    let authorizationData = 'Basic ' + this.b64EncodeUnicode('giovanemere' + ':' + '11a78018ef44bdc4ebcce077bbcefa36eb');
+
+    const uploadData = new FormData();
+
+      uploadData.append('nameFile', this.chosenFileName);
+      uploadData.append('attachedfile', this.chosenFile);
+
+    const headerOptions = {
+
+      headers: new HttpHeaders({
+        'Authorization': authorizationData,
+      })
+
+    };
+
+    return this.http.post(this.serverUrl, uploadData, headerOptions);
+
+  }
+  b64EncodeUnicode(str: string): string {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
+      return String.fromCharCode(('0x' + p1) as any);
+    }));
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nombre: [''],
+      email: [''],
+      nombreResponsable: [localStorage.getItem('UserName')],
+      vicepresidencia: [''],
+      estado: ['Activo'],
+      archivo: ['']
+    });
+  }
+
+  onChange(fileInput: Event) {
+    const control: any = fileInput.target;
+    console.log(fileInput);
+
+
+    if (!control.files || control.length === 0) {
+      this.chosenFileName = null;
+      this.chosenFile = null;
+    } else {
+      this.chosenFileName = control.files[0].name;
+      this.chosenFile = control.files[0];
+      console.log('archivo cargado');
+    }
+    this.viewMode = 'habilitado';
+  }
+
+  onUploadMacro() {
+
+    this.form.get('nombre')?.setValue(this.chosenFileName);
+
+    if (this.chosenFile != undefined) {
+
+        this.login().subscribe((response) => {
+      }
+      );
+    } else {
+      console.log(this.chosenFile);
+    }
+  }
+
 }
 
--- boton/perceptor/src/app/app.module.ts
+++ boton/perceptor/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -15,8 +15,8 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     FormsModule,
     HttpClientModule, // cargamos el módulo en el array de
-    AppRoutingModule
-    
+    AppRoutingModule,
+    ReactiveFormsModule        
   ],
   providers: [],
   bootstrap: [AppComponent]
